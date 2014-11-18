b165fd4c1ebaf1b9df6954d1f88aecddc5a94229
3413 <3413student@lab-3413-05.cuesta.org>
2014-11-17 19:34:07 -0800

    Fixed the layout issue where only the image was in the sidebar, and added simple jquery to add an item to the table.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-var main = function ()
+var testArray = ['test', 'test', 'test', 'test'];
+
+var createRow = function ( elements )
 {
-	alert("Your script is working.");	
+	//Start off with the opening tag to the row.
+	var newRow = "<tr>";
+	
+	//Create the row and add each column data to it.
+	for( var i = 0; i < elements.length; i++ )
+	{
+		newRow = newRow + "<td>" + elements[i]  + "</td>";
+	}
+	//Close off the row.
+	newRow = newRow + "</tr>"
+	
+	return newRow;
+}
+
+
+var addItem = function ()
+{
+	$("#currentPantry tr:last").after( createRow(testArray) );
+	$("#currentPantry tr:last").hide().fadeIn("slow");
 };
 
-main();
+$("#add").click(addItem);
