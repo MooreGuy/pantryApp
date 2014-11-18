46ddd84f785d350fc9b65ae86294121b51cc1ca2
Guy Moore <guy.moore.normal@gmail.com>
2014-11-18 11:39:12 -0800

    Broken implementation of the remove method has been implemented

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 var testArray = ['test', 'test', 'test', 'test'];
 
+
+/*
+	Function creates a new row in to use based on the elements in the
+	array that is passed to it.
+ */
 var createRow = function ( elements )
 {
 	//Start off with the opening tag to the row.
@@ -16,11 +21,30 @@ var createRow = function ( elements )
 	return newRow;
 }
 
-
+/* Adds a new row to the currentPantry table */
 var addItem = function ()
 {
 	$("#currentPantry tr:last").after( createRow(testArray) );
 	$("#currentPantry tr:last").hide().fadeIn("slow");
 };
 
+/* Fades the row out, and then returns the promise object */
+var removeItem = function ()
+{
+	$("#currentPantry tr:last").fadeOut("slow");
+};
+
+/* Attaches the click method to the add button */
 $("#add").click(addItem);
+
+/* Attaches the click method to remove button */
+$("#remove").click
+(
+	removeItem().done
+	(
+		function()
+		{
+			$("#currentPantry tr:last").remove();
+		}
+	)
+);
