c611f21777c405f179340702daee150c96a520ed
Guy Moore <guy.moore.normal@gmail.com>
2014-12-04 03:15:45 -0500

    Made sign up page

---
+++ php/signup.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<!-- Set basic metadata -->
+		<meta charset="utf-8" />
+		<title>Pantry App</title>
+
+		<!-- Import Google Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+
+		<!-- Import CSS -->
+		<link rel="stylesheet" href="../css/main.css" type="text/css" />
+
+	</head>
+
+
+	<body>
+
+		<div id="wrapper" >
+			
+			<div id="mainBody" >
+				
+				<div class="logoBar">
+					<img src="../img/logo.png" alt="Logo" />
+				</div>
+
+				<header>
+
+					<h3>
+						Pantry App
+					</h3>
+				</header>
+
+				<div class="signUpBox" >
+	
+					<h3>
+						Sign Up
+					</h3>
+
+					<form  action="#" method="post">
+						<div>
+							<label for="POST-username">Username</label>
+						</div>	
+						<div>
+							<input id="POST-name" type="text" name="name">
+						</div>
+						<div>
+							<label for="POST-password">Password</label>
+						</div>
+						<div>
+							<input id="POST-password" type="password" name="password">
+						</div>
+						<div>
+							<label for="POST-email">E-Mail</label>
+
+						</div>
+						<div>
+							<input for="POST-email" name="email">
+						</div>
+						<div>
+							<input type="submit" value="Sign Up!">
+						</div>
+					</form>
+
+				</div> <!-- Sign up box -->
+
+				<footer>
+					
+					<p>
+						The Pantry App is open source
+					</p>	
+
+				</footer>
+
+			</div> <!-- mainBody -->
+
+		</div> <!-- wrapper -->
+
+	</body>
+	
+
+</html>
