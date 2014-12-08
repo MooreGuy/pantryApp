af131cb26e38a7522e99cbdd0415eadb8fce5d0a
Guy Moore <guy.moore.normal@gmail.com>
2014-12-08 00:20:15 -0500

    Logo box in index.php added.

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<!-- Set basic metadata -->
+		<meta charset="utf-8" />
+		<title>My Pantry</title>
+
+		<!-- Import Google Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+		
+		<!-- Import roboto condensed -->
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
+
+		<!-- Import CSS -->
+		<link rel="stylesheet" href="css/landing.css" type="text/css" />
+
+	</head>
+
+
+	<body>
+
+
+		<div id="wrapper" >
+			<div class="mainBody" >	
+		
+				<div class="container" >
+
+					<div class="logobox" >
+
+						<h3>Cloud Pantry</h3>
+
+		
+						<img class="left" src="img/logo.png" alt="logo" />
+					
+					</div> <!-- logobox -->
+<a href="https://www.flickr.com/photos/katiecakes/4632470665" title="Pantry Shelves in May by Kathie Lapcevic, on Flickr"><img src="https://farm5.staticflickr.com/4025/4632470665_e860056692_o.jpg" width="3334" height="3168" alt="Pantry Shelves in May"></a>
+					
+				</div> <!-- container -->
+
+				<footer>
+					
+					<p>
+						The Pantry App is open source
+					</p>	
+
+				</footer>
+
+			</div> <!-- mainBody -->
+
+		</div> <!-- wrapper -->
+
+		<!-- Add Jquery and Javascript -->
+		<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js?ver=1.4.2'></script>
+		<script src="js/app.js" ></script> 
+
+	</body>
+	
+
+</html>
